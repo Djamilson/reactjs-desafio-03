6bdd145d204683866a4d42dd0104cfe035e42038
Djamilson <djamilson@gmail.com>
2019-05-24 17:26:32 -0300

    feat: Estrutura do projeto

---
+++ src/App.js
@@ -0,0 +1,18 @@
+ import dotenv from "dotenv";
+import React from "react";
+// import "./config/ReactotronConfig";
+
+//import { Provider } from "react-redux";
+
+// import store from "./store";
+import Routes from "./routes";
+
+dotenv.config();
+
+const App = () => (
+
+    <Routes />
+
+);
+
+export default App;
